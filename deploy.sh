86b48b1837f4a7bb8c17f0f47707067f229c661c
jamiecramb <jamie_cramb@hotmail.com>
2021-07-01 01:26:32 +0100

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t jamiecramb/multi-client:latest -t jamiecramb/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t jamiecramb/multi-server:latest -t jamiecramb/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t jamiecramb/multi-worker:latest -t jamiecramb/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+
+docker push jamiecramb/multi-client:latest
+docker push jamiecramb/multi-server:latest
+docker push jamiecramb/multi-worker:latest
+docker push jamiecramb/multi-client:$GIT_SHA
+docker push jamiecramb/multi-server:$GIT_SHA
+docker push jamiecramb/multi-worker:$GIT_SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=jamiecramb/multi-server:$GIT_SHA
+kubectl set image deployments/client-deployment server=jamiecramb/multi-client:$GIT_SHA
+kubectl set image deployments/worker-deployment server=jamiecramb/multi-worker:$GIT_SHA
